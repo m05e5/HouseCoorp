51108e1e49f7aba6a2e5935441e5e1ddb283af6c
CalebMoses <calebmoses1111@gmail.com>
2020-11-28 22:11:58 +0100

    animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:custom_splash/custom_splash.dart';
 
 
 void main() => runApp(MaterialApp(
-
-)
-  );
+    home: CustomSplash(
+        imagePath: 'assets/houseCorp.jpg',
+        backGroundColor: Colors.deepOrange,
+        animationEffect: 'zoom-in',
+        logoSize: 200,
+        home: HouseCoorp(),
+       // customFunction: duringSplash,
+        duration: 2500,
+        type: CustomSplashType.StaticDuration,
+        //outputAndHome: op,
+    ),
+));
   class HouseCoorp extends StatefulWidget {
     @override
     _HouseCoorpState createState() => _HouseCoorpState();
@@ -14,7 +24,8 @@ void main() => runApp(MaterialApp(
     @override
     Widget build(BuildContext context) {
       return Container(
-        
+        child: Text('hello world'),
+
         
       );
     }
