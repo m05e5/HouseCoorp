1a1ffdc34cbba65591410784e69d62e5ae9250ea
CalebMoses <calebmoses1111@gmail.com>
2020-11-28 21:26:35 +0100

    add

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MaterialApp(
+
+)
+  );
+  class HouseCoorp extends StatefulWidget {
+    @override
+    _HouseCoorpState createState() => _HouseCoorpState();
+  }
+  
+  class _HouseCoorpState extends State<HouseCoorp> {
+    @override
+    Widget build(BuildContext context) {
+      return Container(
+        
+      );
+    }
+  }
